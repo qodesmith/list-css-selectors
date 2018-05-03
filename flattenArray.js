2dabaffade1cf27f70d7258abc8f2b59634c5ac0
Aaron Cordova <theqodesmith@gmail.com>
2018-05-03 16:59:41 -0400

    Fixing flattenArray fxn.

--- flattenArray.js
+++ flattenArray.js
@@ -1,11 +1,10 @@
 function flattenArray(arr) {
-  const isArray = Array.isArray(arr);
-  if (!isArray) return arr;
+  if (!Array.isArray(arr)) return arr;
 
   let finalArray = [];
 
   arr.forEach(thing => {
-    if (isArray) {
+    if (Array.isArray(thing)) {
       finalArray = finalArray.concat(flattenArray(thing));
     } else {
       finalArray.push(thing);
